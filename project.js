82c25a2ae00f3f0f4d652bcc7be52c351349a706
Venkatesh <vullagantivenkatesh@gmail.com>
2022-08-10 19:42:37 +0530

    Added new products html and css page

--- project.js
+++ project.js
@@ -1,11 +1,22 @@
 // const slides = document.querySelectorAll(".slide");
 
-// const slider = document.querySelector(".images-slider");
-// slider.style.transform = "scale(0.5)";
-// slider.style.overflow = "visible";
+// // const slider = document.querySelector(".images-slider");
+// // slider.style.transform = "scale(0.5)";
+// // slider.style.overflow = "visible";
+
+// const btnLeft = document.querySelector(".btn-left");
+// const btnRight = document.querySelector(".btn-right");
+// let currentSlide = 0;
 
 // slides.forEach(
-//   (slide, index) => (slide.style.transform = `translate(${100 * index})`)
+//   (slide, index) => (slide.style.transform = `translateX(${100 * index}%)`)
 // );
+// // 0% 100% 200% ..
 
-// 0% 100% 200% ..
+// btnRight.addEventListener("click", function () {
+//   currentSlide++;
+//   slides.forEach(
+//     (slide, index) =>
+//       (slide.style.transform = `translateX(${100 * (index - currentSlide)}%)`)
+//   );
+// });
